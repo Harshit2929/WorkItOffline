d6a4628c82f8d65d897c806097c95b4df64c65f6
harshitFactors <97429680+harshitFactors@users.noreply.github.com>
2022-08-20 11:50:38 +0530

    some minor changes

--- backend/migration/4_add_names_to_shg.sql
+++ backend/migration/4_add_names_to_shg.sql
@@ -1,5 +1,5 @@
 USE server_database;
 ALTER TABLE SHG
 ADD first_name varchar (255),
-ADD last_name varchar (255),
-ADD middle_name varchar(255)
+    ADD last_name varchar (255),
+    ADD middle_name varchar(255)
