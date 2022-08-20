21cd0c11f5bf717efeb7b6ab60c1f5aa1d240174
Himanshu Pandey <himanshu.dn.pandey@gmail.com>
2022-08-20 16:58:36 +0530

    Add connection.js changes
    Signed-off-by: Himanshu Pandey <himanshu.dn.pandey@gmail.com>

--- backend/connection.js
+++ backend/connection.js
@@ -1,5 +1,4 @@
-const mysql=require('mysql')
-
+//const mysql=require('mysql')
 // const db = mysql.createConnection({
 //     host     : 'postgresdb',
 //     user     : 'root', 
@@ -22,4 +21,29 @@ const mysql=require('mysql')
   //   }
   // })
 
-  module.exports={}
+require("dotenv").config();
+const Sequelize = require("sequelize");
+
+const options = {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+  // logging: console.log,
+  force: process.env.DB_FORCE_RESTART
+}
+
+const connection = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  options
+);
+
+(async () => {
+  try {
+    await connection.sync();
+  } catch (error) {
+    throw new Error(error);
+  }
+})();
+
+module.exports = connection;
