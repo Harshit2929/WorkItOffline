9b2e019a2d51fb6e93116b5682062b95ddb53d9b
Priyanshi Chandra <chandrapriyanshi@gmail.com>
2022-08-26 16:28:33 +0530

    commit

--- app/lib/shglist.dart
+++ app/lib/shglist.dart
@@ -5,6 +5,7 @@ void main() {
   runApp(const MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -52,7 +53,87 @@ class HomePage extends StatelessWidget {
                   ),
                 ))).toList()
         )
+        
 
     );
   }
+}
+
+class User{
+  final String id;
+  final String title;
+  final String subtitle;
+  
+  const User({
+    required this.id,
+    required this.title,
+    required this.subtitle
+  });
+   static User fromJson(json) =>   User(
+       id: json['id'],
+       title: json['title'],
+       subtitle: json['subtitle']
+   );
+}
+
+class MainPage extends StatefulWidget{
+    State<MainPage> createState()=>_MainPage();
+}
+
+class _MainPage extends State<MainPage> {
+
+  List<User> users = getUsers();
+
+  static List<User> getUsers() {
+    const data = [
+      {
+        "id": 'SHG_id1',
+        "title": 'SHG_Title1',
+        "subtitle": 'Surat, Gujarat'
+      },
+      {
+
+        "id": 'SHG_id2',
+        "title": 'SHG_Title2',
+        "subtitle": 'Delhi'
+      },
+    ];
+    return data.map<User>(User.fromJson).toList();
+  }
+
+  @override
+  
+  Widget build(BuildContext context) {
+
+
+
+
+    body:Center(
+
+      child: buildUsers(users),
+    );
+    Widget buildUsers(List<User> users) {
+       return ListView.builder(
+         itemCount: users.length,
+         itemBuilder: (context, index) {
+           final user = users[index];
+           return ListTile(
+             leading: CircleAvatar(
+               backgroundColor: Colors.amber,
+               child: Text(user.id.toString()),
+             ),
+             title: Text(user.title),
+             subtitle: Text(user.subtitle),
+             trailing: IconButton(
+               icon: const Icon(Icons.add_a_photo),
+               onPressed: () {},
+             ),
+           );
+         },    //itembuilder
+
+       );
+
+     }     //buildUsers
+
+  }  //build widget
 }
