04eb65ef367e3705e6efa31af488c652dfeb5350
HARSHIT2929 <harshitchauhan12102001@gmail.com>
2022-08-26 06:28:50 +0530

    some minor changes

--- backend/routes/activity.route.js
+++ backend/routes/activity.route.js
@@ -5,6 +5,6 @@ const activity = require("./../controllers/activity.controller")
 //create a meeting
 router.post('/', activity.create);
 //get all acticities for a shg
-router.get('/:shg', activity.findAll);
+router.get('/:shg_id', activity.findAll);
 
 module.exports = router;
